bdbb60ef9130e64176886d900bbdbc9b4345eb40
Tech-gig <122024987+Tech-gig@users.noreply.github.com>
2025-03-11 04:19:54 -0700

    Update hw.py

--- hw.py
+++ hw.py
@@ -8,27 +8,36 @@ display.set_caption('Frogger-Game ') #Set title of window
 endGame = False
 PictureFrog = image.load("frog.png")
 PictureFrog = transform.scale(PictureFrog,(40,40))#Sets the measurement for picture 
-RectFrog = Rect(100,0,40,40)
+RectFrog = Rect(100,0,40,40)#Draws a rectangle to the screen 
+PictureCar = image.load("car.png")
+PictureCar = transform.scale(PictureCar, (50,50))
+cars = []
+cars.append(Rect(200,200,40,40))
+
 
 while endGame == False:
 	for e in event.get(): # It is the gameloop 
 		if e.type == QUIT:
-			endGame = True
+			endGame = True #This quits the game 
 		elif e.type == KEYDOWN:
 			if e.key == K_s:
 				RectFrog.move_ip(0,5)
 			elif e.key == K_w:
-				RectFrog.move.ip(0,-5)
+				RectFrog.move_ip(0,-5)
 			elif e.key == K_d:
 				RectFrog.move_ip(5,0)
 			elif e.key == K_a:
 				RectFrog.move_ip(-5,0)
+	for c in cars:
+		if c.colliderect(RectFrog):
+			endGame=True 
 				
 				
 				
 				
 	screen.fill((0,0,0))
-	draw.rect(screen, (255,255,255), (0,0,width,20))
+	for c in cars:
+		draw.rect(screen, (255,255,255), c)
 	screen.blit(PictureFrog, RectFrog)
 	display.update()#Updates portions of screen for software updates 
 	time.delay(30)
